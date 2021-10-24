a0c8f1669ad90f73fb4c84864b5055b5ab90687c
shrutidubey2001 <90273648+shrutidubey2001@users.noreply.github.com>
2021-10-25 01:11:41 +0530

    Add files via upload

---
+++ System/admin/aboutus.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <title>About</title>
+<link rel="stylesheet" href="../csss/aboutus.css" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Flamenco" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+
+</head>
+<body>
+    <header>
+      <nav>
+        <div class="row clearfix">
+          
+            <ul class="main-nav" animate slideInDown>
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="aboutus.php">About Us</a></li>
+                <li><a href="contactus.php">Contact Us</a></li>
+                <li><a href="../login.php">Admin Login</a></li>
+          </ul>
+        </div>
+      </nav>
+      <div class="main-content-header">
+          <h2 class="search">About Us</h2>
+             <div class="div2">
+
+             <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+             industry. Lorem Ipsum has been the industry's standard dummy text ever
+             since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              It has survived not only five centuries, but also the leap into electronic typesetting, remaining
+              essentially unchanged. It was popularised in the 1960s</p>
+             </div>
+          </div>
+    </header>
+
+</body>
+</html>
